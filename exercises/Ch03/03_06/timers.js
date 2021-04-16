3315d8815ee85fa962fe30c123b1d34cdb706e7d
Erika Martinez <17046460+erikamart@users.noreply.github.com>
2021-04-15 22:15:52 -0700

    updated notes for the rest of Ch03

--- exercises/Ch03/03_06/timers.js
+++ exercises/Ch03/03_06/timers.js
@@ -1,3 +1,7 @@
+// CREATING A DELAY WITH setTimeout
+
+// Timing functions are also asynchronous functions. There is  set timeout, clear timeout, set interval, and clear interval. They work the same way they do in the browser and are available globally. Create a 3 second delay using the setTimeout function. Line 5 is a new constant variable assigned to 3000ms which is equivalent to 3s. Line 6 uses the console log to output a message using a template string in backticks. The string contains the waitTime function divided by 1000 to convert the miliseconds to seconds so it will be able to show the number of seconds that the user is waiting for. Line 8 is a one line arrow function that, once invoked, will log the word "done" to signal the timer is finished. Line 10 is the setTimeout function with a 1st argument that is the function to invoke the timerFinished variable on line 8. It's 2nd argument is the function that invokes the time to wait for this delay, or line 5. Test this code in the terminal with "~$ node timers". You should see this process run asynchronously for three seconds. Once it has waited for three seconds, the timer finished function will be invoked and the word "done" should be logged to the console before it exits the terminal. 
+
 const waitTime = 3000;
 console.log(`setting a ${waitTime / 1000} second delay`);
 
--- exercises/Ch03/03_08/timers.js
+++ exercises/Ch03/03_08/timers.js
@@ -1,4 +1,8 @@
-const waitTime = 3000;
+// REPORT PROGRESS WITH setInterval
+
+// To improve upon the code written in the previous chapter, you can report the waiting time as a percentage. Within the increment time function on lines 9-15, you see line 11. This line calculates a percentage by declaring a constant variable called "p" and then assigning it to the calculation that uses the math.floor() function. You use this function because the resulting number will be a decimal value and this helps give a percentage by trimming off the decimal part of the number. Then line 12 uses process.stdout to clear the last line written to the terminal. Line 13 will move the cursor back to the start position of the terminal. Note that lines 21 & 22 in the timerFinished function will also utilize these functions to do the same. Line 14 is the resulting output message that gives the percentage.
+
+const waitTime = 5000;
 const waitInterval = 500;
 let currentTime = 0;
 
@@ -22,6 +26,6 @@ const timerFinished = () => {
 const interval = setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
 
-// So we have the interval that we've used using set interval on line 22. The interval is set to call the increment time function that we declared on line five every half-second. So every half-second we're going to add to the current time and then calculate our percentage using the current time versus the wait time, which is the total time. Using process standard output we're able to clear the line, move the cursor back to zero, and then overwrite our last percentage with the new value.
+// In summary, the code uses set interval on line 26. The interval is set to call the increment time function declared on line 9 for every half-second. Every half-second it is going to add to the current time and then calculate the percentage using the current time versus the wait time, which is the total time. Using process standard output you are able to clear the line, move the cursor back to zero, and then overwrite the last percentage with the new value.
 
-// Once the timer has finished on line 15 we declare the timer finished function that clears the interval and then removes the last percentage line so that we can write done in its place. 
+// Once the timer has finished on line 19 you declare the timer finished function that clears the interval and then removes the last percentage line so that it can write done in its place. 
